30282131dd5515c3701925969f2812196ee8c518
Antoine Debes <antoinedebes@gmail.com>
2021-06-30 16:33:30 +0300

    Header-component layout is done

---
+++ src/components/header/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Header.css";
+import Logo from "../../images/image.jpg";
+// import Icon from "../../icon";
+
+function Header() {
+  return (
+    <div className="wrapper" id="wrapper">
+      <div className="header">
+        <div className="header-container">
+          <div className="header-container__search">
+            {/* <div>
+              <Icon icon="search" size={20} />
+            </div> */}
+            <div>
+              <input
+                type="text"
+                className="header-container__search--input"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+          <div className="header-container__logo">
+            <img src={Logo} alt="" className="header-container__logo--image" />
+          </div>
+          <div className="header-container__cart">
+            <button className="header-container__cart--btn">Cart</button>
+          </div>
+        </div>
+        <div className="header__navbar">
+          <ul className="header__navbar--ul">
+            <li>Home</li>
+            <li>Shop Men</li>
+            <li>Shop Women</li>
+            <li>Sale</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
