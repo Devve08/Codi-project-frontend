57dc9ba0ec95707ed9d89e105b30602740da1bc3
Antoine Debes <Antoinedebes@gmail.com>
2021-07-19 10:01:23 +0300

    Changed the app.js routing layout

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [usernameToken, setUsernameToken] = useState(false);
 
   useEffect(() => {
-    let localToken = localStorage.getItem("token");
+    let localToken = localStorage.getItem("token"); // Token Auth
     Axios.post("http://localhost:4000/user/login", {
       token: localToken,
     })
